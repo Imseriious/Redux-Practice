3f38166b9b261d7e3cd6b32f4e5c373f7d676628
Sammy.p <samueledi80@hotmail.com>
2019-09-29 18:04:28 +0200

    action added to the store

--- redux-basics.js
+++ redux-basics.js
@@ -1,7 +1,7 @@
 const redux = require('redux');
 const createstore = redux.createStore; //Creates Store
 
-const initialState = { 
+const initialState = {
     counter: 0
 }
 
@@ -9,13 +9,28 @@ const initialState = {
 //There is just 1 reducer even if we combine multiple ones 
 //Only thing that may update the state
 const rootReducer = (state = initialState, action) => { //We asign the initial state
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state, //Always copy the state don't mutate
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
 //Store // Needs to be initialized with a Reducer
-const store = createstore(rootReducer); 
+const store = createstore(rootReducer);
 console.log(store.getState());
 
-//Displatching Action
+//Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 
+console.log(store.getState());
 //Subscription
