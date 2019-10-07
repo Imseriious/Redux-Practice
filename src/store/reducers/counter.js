c3c5ecdb2f270c76fd37a42b6e8e82ca8680729c
Sammy.p <samueledi80@hotmail.com>
2019-10-07 14:25:12 +0200

    Middleware and Redux extension added

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -7,10 +7,9 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionType.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
         case actionType.DECREMENT:
             return {
                 ...state,
